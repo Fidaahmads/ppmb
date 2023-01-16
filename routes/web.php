803c9bd8954e3808e9e3e89980bddd3d1ddf8886
Fidaahmads <fidahmadsyuhada@gmail.com>
2023-01-16 09:35:32 +0700

    tutorialSK
    Signed-off-by: Fidaahmads <fidahmadsyuhada@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Controller;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/', [Controller::class, 'index']);
-
-//function () {
-    //return view('welcome');
-//});
+//route resource
+Route::resource('/posts', \App\Http\Controllers\PostController::class);
