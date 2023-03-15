448bb0490c2d48aa1806f2aa8345768127a7ce37
Fidaahmads <fidahmadsyuhada@gmail.com>
2023-03-15 10:00:35 +0700

    vue - survey pelanggan
    Signed-off-by: Fidaahmads <fidahmadsyuhada@gmail.com>

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-import './bootstrap';
+//import './jquery';
+//import './bootstrap';
+
+import {createApp} from 'vue'
+
+import App from './App.vue'
+
+const app = createApp(App)
+
+app.mount('#appli')
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\SurveyController;
 
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/surveys', [SurveyController::class, 'index']);
+Route::post('/surveys', [SurveyController::class, 'store']);
 
 //route resource
 Route::post('login', [ 'as' => 'login', 'uses' => 'LoginController@login']);
@@ -15,10 +18,17 @@ Route::middleware(['auth', 'user-access'])->group(function () {
     Route::resource('members', App\Http\Controllers\MemberController::class);
     Route::resource('schedules', App\Http\Controllers\ScheduleController::class);
     Route::resource('presences', App\Http\Controllers\PresenceController::class);
-    Route::resource('quizz/quizzes', App\Http\Controllers\quizz\QuizzesController::class);
 
 });
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/', function () {
+    return view('app');
+});
+
+Route::get('/{any}', function () {
+    return view('app');
+})->where('any','.*');
 
